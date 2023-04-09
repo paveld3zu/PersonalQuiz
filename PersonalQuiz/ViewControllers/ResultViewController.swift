0d56a1994bba2c8306f1321b27ba27eef222b001
paveld3zu <pavel.karunnyj@yandex.ru>
2023-04-09 11:42:03 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 06.04.2023.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+    
+    var answersChosen: [Answer]!
+    
+    @IBOutlet var iconLabel: UILabel!
+    @IBOutlet var yourResultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        calculateResult()
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    private func calculateResult() {
+        var animals: [Animal: Int] = [:]
+        var answers: [Animal] = []
+        
+        for answer in answersChosen {
+            answers.append(answer.animal)
+        }
+        
+        for answer in answers {
+            if let count = animals[answer] {
+                animals[answer] = count + 1
+            } else {
+                animals[answer] = 1
+            }
+        }
+        
+        var greaterValue: Animal
+        var maxCount = 1
+        for (answer, count) in animals {
+            if count > maxCount {
+                maxCount = count
+                greaterValue = answer
+                iconLabel.text = "Вы - \(greaterValue.rawValue)"
+                yourResultLabel.text = greaterValue.definition
+            }
+        }
+    }
+}
+
